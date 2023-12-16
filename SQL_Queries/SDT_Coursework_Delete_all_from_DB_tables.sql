88a06340e651b2194bdf292553fa99c7dfc5a5f2
Denys <d.d.kublik@gmail.com>
2023-12-16 16:32:23 +0200

    Completed "Create" and "Insert" SQL Queries with 3 additional scripts  "Delete all", "Select all" and "Reseed"

---
+++ SQL_Queries/SDT_Coursework_Delete_all_from_DB_tables.sql
@@ -0,0 +1,18 @@
+USE SMART_HEAT;
+
+-- Remove all data from the tables
+DELETE FROM Maintenance;
+DELETE FROM Staff;
+DELETE FROM Administrators;
+DELETE FROM OSBB_Participants;
+DELETE FROM ORS_Participants;
+DELETE FROM OMS_Participants;
+DELETE FROM Users;
+DELETE FROM PersonData;
+DELETE FROM Indicators;
+DELETE FROM Sensors;
+DELETE FROM SensorTypes;
+DELETE FROM Addresses;
+DELETE FROM Building_types;
+DELETE FROM Streets;
+DELETE FROM Districts;
---
+++ SQL_Queries/SDT_Coursework_Select_All_from_DB_tables.sql
@@ -0,0 +1,18 @@
+USE SMART_HEAT;
+
+-- Select all records from the tables
+SELECT * FROM Districts;
+SELECT * FROM Streets;
+SELECT * FROM Building_types;
+SELECT * FROM Addresses;
+SELECT * FROM SensorTypes;
+SELECT * FROM Sensors;
+SELECT * FROM Indicators;
+SELECT * FROM PersonData;
+SELECT * FROM Users;
+SELECT * FROM OMS_Participants;
+SELECT * FROM ORS_Participants;
+SELECT * FROM OSBB_Participants;
+SELECT * FROM Administrators;
+SELECT * FROM Staff;
+SELECT * FROM Maintenance;
