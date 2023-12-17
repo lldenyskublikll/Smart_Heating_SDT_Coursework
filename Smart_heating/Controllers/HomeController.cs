a0a440389fc31b181d445538bc09a9006958db88
Denys <d.d.kublik@gmail.com>
2023-12-17 17:14:01 +0200

    Created Smart_Heating Apllication. Connectd DB to the project. Added models of DB tables and views

---
+++ Smart_heating/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Smart_heating.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Про цей застосунок";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Зворотній зв'язок";
+
+            return View();
+        }
+
+        public ActionResult LogIn() 
+        {
+            return View();        
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login() 
+        {
+            
+        }
+    }
+}
---
+++ Smart_heating/Models/UserValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Smart_heating.Models
+{
+    [MetadataType(typeof(UserMetaData))]
+    public partial class User 
+    {
+        [Bind(Exclude = "UserID")]
+        public class UserMetaData
+        {
+            [ScaffoldColumn(false)]
+            public int UserID { get; set; }
+
+            // checking the accuracy of entered login
+            [DisplayName("Логін")]
+            [Required(ErrorMessage = "Потрібно ввести свій логін")]
+            [MinLength(10, ErrorMessage = "Логін замалий. Мінімум 10 символів")]
+            [MaxLength(20, ErrorMessage = "Логін завеликий. Максимум 20 символів")]
+
+            public string UserLogin { get; set; }
+
+            // checking the accuracy of entered password
+            [DisplayName("Пароль")]
+            [Required(ErrorMessage = "Потрібно ввести свій пароль")]
+            [MinLength(10, ErrorMessage = "Пароль замалий. Мінімум 10 символів")]
+            [MaxLength(20, ErrorMessage = "Пароль завеликий. Максимум 20 символів")]
+
+            public string UserPassword { get; set; }
+        }
+    }
+}
