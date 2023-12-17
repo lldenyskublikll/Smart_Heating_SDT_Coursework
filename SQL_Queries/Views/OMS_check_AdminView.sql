ae2ea325b624565dbc0aae61d69c0b002c88ed83
Dekkan <100539102+lldenyskublikll@users.noreply.github.com>
2023-12-17 12:38:30 +0200

    Update OMS_check_AdminView.sql

--- SQL_Queries/Views/OMS_check_AdminView.sql
+++ SQL_Queries/Views/OMS_check_AdminView.sql
@@ -1,10 +1,10 @@
 Create view OMS_check_AdminView as
-Select Us.UserID as 'User ID', Us.UserLogin as 'Логін', Oms.OMSUSerID as 'ÎÌÑ User ID', 
-       Pd.Surname as 'Ïð³çâèùå', Pd.PrsnName as '²ì`ÿ', 
-       ISNULL(Pd.SecondName, 'Â³äñóòíº') as 'Ïî-Áàòüêîâ³',
-	   Pd.Gender as 'Ñòàòü', Pd.BirthDate as 'Äàòà íàðîäæåííÿ', Pd.PhoneNumber as 'Í-ð òåëåôîíó',	   
-	   ISNULL(Pd.RsrvPhoneNumber, 'Íå âêàçàíî') as 'Ðåçåðâíèé í-ð òåëåôîíó',
-	   St.StreetName as 'Íàçâà âóëèö³', Ds.DistrictName as 'Ðàéîí', Adr.House as '¹ Áóäèíêó'
+Select Us.UserID as 'User ID', Us.UserLogin as 'Логін', Oms.OMSUSerID as 'ОМС User ID', 
+       Pd.Surname as 'Прізвище', Pd.PrsnName as 'Ім`я', 
+       ISNULL(Pd.SecondName, 'Відсутнє') as 'По-Батькові',
+	   Pd.Gender as 'Стать', Pd.BirthDate as 'Дата народження', Pd.PhoneNumber as 'Н-р телефону',	   
+	   ISNULL(Pd.RsrvPhoneNumber, 'Не вказано') as 'Резервний н-р телефону',
+	   St.StreetName as 'Назва вулиці', Ds.DistrictName as 'Район', Adr.House as '№ Будинку'
 From OMS_Participants Oms
 Join PersonData Pd on Pd.PersonID = Oms.PersonID
 Join Users Us on Us.PersonID = Pd.PersonID
